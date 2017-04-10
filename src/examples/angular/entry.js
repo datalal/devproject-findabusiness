8485c5301bae39032db74bebdbe48dcb20b0d891
datalal <lawsonw2@gmail.com>
2017-04-10 00:22:44 -0400

    beautify

--- src/examples/angular/entry.js
+++ src/examples/angular/entry.js
@@ -2,8 +2,8 @@
 const angular = require('angular')
 
 // Create a template
-const helloWorldTemplate = `
-<div ng-controller="helloWorldController">
+const searchTemplate = `
+<div ng-controller="searchController">
   <div class="alert alert-success">
     <form>
       <input type="text" placeholder="Business Name" ng-model="bName">  <input type="text" placeholder="Zip Code" ng-model="zip">
@@ -21,63 +21,62 @@ const helloWorldTemplate = `
 `
 
 // Create a module
-const exampleModule = angular.module('exampleModule', [])
+const searchModule = angular.module('searchModule', [])
 
 // Create and register a controller
-const HelloWorldController = ($scope) => {
+const searchController = ($scope) => {
 
 
-  $scope.placePhotoArray = [];
-
-
-
-
-  var mapDiv = document.createElement('div');
-  var map = new google.maps.Map(mapDiv);
-
-  var service = new google.maps.places.PlacesService(map);
-
-
-$scope.submit = function () {
-$scope.queryString = $scope.bName + ' ' + $scope.zip;
-console.log($scope.queryString);
-service.textSearch({
-   query: $scope.queryString
- }, function(results) {
-   console.log(results);
-  //  console.log(results[0].place_id);
-  $scope.placeId = results[0].place_id;
+    $scope.placePhotoArray = [];
 
 
 
-   service.getDetails({
-             placeId: $scope.placeId
-           }, function(place, status) {
-             if (status === google.maps.places.PlacesServiceStatus.OK) {
-               $scope.placeRes = place;
-            console.log(place);
-            for (var i = 0; i < $scope.placeRes.photos.length; i++){
-            $scope.placePhotoArray.push($scope.placeRes.photos[i].getUrl(
-              {maxWidth: 500,
-              maxHeight: 500}))
-            };
-            $scope.$apply();
-             }
-           });
+
+    var mapDiv = document.createElement('div');
+    var map = new google.maps.Map(mapDiv);
+
+    var service = new google.maps.places.PlacesService(map);
 
 
- });
+    $scope.submit = function() {
+        $scope.queryString = $scope.bName + ' ' + $scope.zip;
+        console.log($scope.queryString);
+        service.textSearch({
+              query: $scope.queryString
+            }, function(results) {
+              console.log(results);
+              $scope.placeId = results[0].place_id;
+
+    service.getDetails({
+        placeId: $scope.placeId
+        }, function(place, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          $scope.placeRes = place;
+          console.log(place);
+          for (var i = 0; i < $scope.placeRes.photos.length; i++) {
+            $scope.placePhotoArray.push($scope.placeRes.photos[i].getUrl({
+              maxWidth: 500,
+              maxHeight: 500
+            }))
+          };
+          $scope.$apply();
+        }
+      });
+
+
+    });
 }
 
 }
-exampleModule.controller('helloWorldController', HelloWorldController)
+
+searchModule.controller('searchController', searchController)
 
 // Adding the controller to the DOM
 // In your use, it's entirely suitable to have this directly in the index.html.
 // It's only contained here for example purposes so a clean index.html is preserved.
 const appContainerEl = document.createElement('div')
-appContainerEl.innerHTML = helloWorldTemplate
+appContainerEl.innerHTML = searchTemplate
 document.body.appendChild(appContainerEl)
 
 // Bootstrapping the Angular app
-angular.bootstrap(appContainerEl, ['exampleModule'])
+angular.bootstrap(appContainerEl, ['searchModule'])
